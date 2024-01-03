773177e17ef40cd18ba584ff6b244711ada15d53
heziyu2025 <ziyu2025@gmail.com>
2024-01-03 12:11:01 +0800

    Added the ability to display images on run_model.py

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # ImageAssorting
+
+## Installing
+1. Clone repository
+```cmd
+git clone https://github.com/heziyu2025/ImageAssorting.git
+cd ./ImageAssorting.git
+```
+2. Download and install [python](https://www.python.org/)
+3. Install [pytorch](https://pytorch.org/) (If you have a nvidia gpu, you can use [cuda](https://developer.nvidia.com/cuda-toolkit) for higher speed)
+
+## Training
+
+To train the model, run:
+
+```cmd
+python3 ./train/train.py
+```
+
+## Usage
+
+To run the model, run:
+
+```cmd
+python3 ./run_model.py
+```
+
--- run_model.py
+++ run_model.py
@@ -1,7 +1,8 @@
-from model import NeuralNetwork, device
-from dataset import test_data
+from train.model import NeuralNetwork, device
+from train.dataset import test_data
 
 import torch
+import matplotlib.pyplot as plt
 
 model = NeuralNetwork().to(device)
 model.load_state_dict(torch.load("models/model.pth"))
@@ -19,10 +20,23 @@ classes = [
     "Ankle boot",
 ]
 
+index = 3
+
 model.eval()
-x, y = test_data[0][0], test_data[0][1]
+x, y = test_data[index][0], test_data[index][1]
+
 with torch.no_grad():
     x = x.to(device)
     pred = model(x)
     predicted, actual = classes[pred[0].argmax(0)], classes[y]
+
+
+    x_np = x.cpu().numpy()
+    x_np = x_np.transpose((1, 2, 0))
+
+    plt.imshow(x_np)
+    plt.title(f'Predicted: "{predicted}", Actual: "{actual}"')
+    plt.axis('off')
+    plt.show()
+
     print(f'Predicted: "{predicted}", Actual: "{actual}"')
